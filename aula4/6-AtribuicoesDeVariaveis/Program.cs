d532601b65d67eca025e2be16bf159b2e2c706e0
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-04-23 10:15:38 -0300

    Aula 4

---
+++ aula4/6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 6");
+
+            int idade = 32;
+            int idadeGustavo = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeGustavo);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ aula4/7-CondicionaisEControleDeFluxo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_CondicionaisEControleDeFluxo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 7");
+
+            int idade = 15;
+            int numeroPessoas = 2;
+
+            if (idade >= 16)
+            {
+                Console.WriteLine("Bem-vindo, você pode entrar!");
+            }
+            else if (numeroPessoas >= 2)
+            {
+                Console.WriteLine("Você não tem 16 ou mais de idade, mas está acompanhado. Pode entrar!");
+            }
+            else
+            {
+                Console.WriteLine("Infelizmente você não pode entrar pois não tem 16 e não está acompanhado");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
